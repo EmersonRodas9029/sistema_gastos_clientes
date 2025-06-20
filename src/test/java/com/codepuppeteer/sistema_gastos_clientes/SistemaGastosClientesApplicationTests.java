4301494c47ac1b8faa9916902cb283af2e9abfc4
theri <theripper446@gmail.com>
2025-06-20 15:08:35 -0600

    "Configuración de base de datos MySQL con Docker Compose"

---
+++ src/test/java/com/codepuppeteer/sistema_gastos_clientes/SistemaGastosClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepuppeteer.sistema_gastos_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGastosClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
